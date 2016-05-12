46f5ba8c89369ef63b1f162a1235b15dfc462e56
Eugene Baibourine <eugene.baibourine@ihg.com>
2016-05-12 14:05:48 -0600

    finally got the text from the wheel. The rest is just formatting and simple logic.

--- wheel-demo/app/src/main/java/kankan/wheel/demo/extended/HoloWheelActivity.java
+++ wheel-demo/app/src/main/java/kankan/wheel/demo/extended/HoloWheelActivity.java
@@ -1,5 +1,7 @@
 package kankan.wheel.demo.extended;
 
+import kankan.wheel.widget.OnWheelChangedListener;
+import kankan.wheel.widget.OnWheelScrollListener;
 import kankan.wheel.widget.WheelView;
 import kankan.wheel.widget.adapters.AbstractWheelTextAdapter;
 
@@ -8,9 +10,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class HoloWheelActivity extends Activity {
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,8 @@ public class HoloWheelActivity extends Activity {
         books.setShadowColor(0xFF000000, 0x88000000, 0x00000000);
         books.setViewAdapter(new BookAdapter(this));
         books.setCurrentItem(3);
+        books.addChangingListener(changedListener);
+        books.addScrollingListener(scrolledListener);
 
         final WheelView time = (WheelView) findViewById(R.id.time_length);
         time.setVisibleItems(3);
@@ -33,15 +39,67 @@ public class HoloWheelActivity extends Activity {
         time.setShadowColor(0xFF000000, 0x88000000, 0x00000000);
         time.setViewAdapter(new TimeAdapter(this));
         time.setCurrentItem(2);
+        time.addScrollingListener(scrolledListener);
+        time.addChangingListener(changedListener);
 
         final WheelView digits = (WheelView) findViewById(R.id.digits);
         digits.setVisibleItems(3);
         digits.setWheelBackground(R.drawable.wheel_bg_holo);
         digits.setWheelForeground(R.drawable.wheel_val_holo);
         digits.setShadowColor(0xFF000000, 0x88000000, 0x00000000);
+        //Built in numeric adapter but it has a different style
+//        digits.setViewAdapter(new NumericWheelAdapter(this, 1, 30));
         digits.setViewAdapter(new NumberAdapter(this));
         digits.setCurrentItem(2);
+        digits.addChangingListener(changedListener);
+        digits.addScrollingListener(scrolledListener);
 
+        //force update on initial app open
+//        updateText();
+
+    }
+
+    private boolean wheelScrolled = false;
+
+    OnWheelScrollListener scrolledListener = new OnWheelScrollListener() {
+        @Override
+        public void onScrollingStarted(WheelView wheelView) {
+            wheelScrolled = true;
+        }
+
+        @Override
+        public void onScrollingFinished(WheelView wheelView) {
+            wheelScrolled = false;
+            updateText();
+        }
+    };
+
+    OnWheelChangedListener changedListener = new OnWheelChangedListener() {
+        @Override
+        public void onChanged(WheelView wheelView, int i, int i1) {
+            if(!wheelScrolled){
+                updateText();
+            }
+        }
+    };
+
+    private void updateText(){
+        TextView finalText = (TextView) findViewById(R.id.display_text);
+        final WheelView books = (WheelView) findViewById(R.id.book);
+        final WheelView time = (WheelView) findViewById(R.id.time_length);
+        final WheelView digits = (WheelView) findViewById(R.id.digits);
+
+        BookAdapter bookAdapter = new BookAdapter(this);
+        TimeAdapter timeAdapter = new TimeAdapter(this);
+        NumberAdapter numberAdapter = new NumberAdapter(this);
+
+
+        int totalDays, pages, days = 0;
+
+        String booksText = bookAdapter.getItemText(books.getCurrentItem()).toString();
+        String timeText = timeAdapter.getItemText(time.getCurrentItem()).toString();
+        String digitsText = numberAdapter.getItemText(digits.getCurrentItem()).toString();
+        finalText.setText("You're trying to read " + booksText + " in " + digitsText +  " " + timeText);
     }
 
     private class NumberAdapter extends AbstractWheelTextAdapter {
